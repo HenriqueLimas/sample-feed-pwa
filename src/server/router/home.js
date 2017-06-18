1526240ee7b92d05fdd545e8a845c8e20bb631d2
HenriqueLimas <henrique.ramos.limas@gmail.com>
2017-06-18 12:27:14 -0300

    Add main headline

--- src/server/router/home.js
+++ src/server/router/home.js
@@ -1,5 +1,11 @@
 const Home = require('../../views').Home
 
 module.exports = function home (req, res) {
-  res.send(Home({ scripts: ['app.js'] }))
+  res.send(Home({
+    scripts: ['app.js'],
+    headline: {
+      title: 'Snowing hard this week',
+      subtitle: 'Montepelier will have a lot of snow today'
+    }
+  }))
 }
--- src/views/home.js
+++ src/views/home.js
@@ -1,10 +1,25 @@
 import Header from './partials/header.js'
 import Footer from './partials/footer.js'
 
-const Home = (ctx) => `
-  ${Header({ title: 'Home' })}
+import Like from './components/actions/like'
+import Bookmark from './components/actions/bookmark'
+import Share from './components/actions/share'
 
-  <h3>This is the Home</h3>
+const Home = (ctx) => `
+  ${Header({ title: 'NewsInCity' })}
+
+  <section class="nic-main-headline">
+    <a class="nic-main-headline__image" href="${ctx.headline.url}"></a>
+    <div class="nic-main-headline__details">
+      <h2 class="nic-main-headline__title">${ctx.headline.title}</h2>
+      <h3 class="nic-main-headline__subtitle">${ctx.headline.subtitle}</h3>
+      <div class="nic-main-headline__actions nic-actions">
+        ${Like()}
+        ${Bookmark()}
+        ${Share()}
+      </div>
+    </div>
+  </section>
 
   ${Footer({ title: 'Home', scripts: ctx.scripts })}
 `
