4e6d7b0ee376903b14dcbd5278c506f8816cb0ba
HenriqueLimas <henrique.ramos.limas@gmail.com>
2017-06-21 13:48:46 -0300

    Fix/Improve FP helpers

--- src/client/scripts/utils/fp.js
+++ src/client/scripts/utils/fp.js
@@ -1,46 +1,74 @@
-export const bind = (fn, ...args) => {
-  function curriedBind(o, ...args) {
-    return fn.bind.apply(fn, [o].concat(args))
+const functionLength = f => f._length || f.length
+
+export const bind = function bind(fn) {
+  function curriedBind(o) {
+    const args = [].slice.call(arguments, 1)
+    const g = fn.bind.apply(fn, [o].concat(args))
+
+    g._length = Math.max(functionLength(fn) - args.length, 0)
+
+    return g
   }
 
-  return args.length
-    ? curriedBind.apply(this, args)
+  return (arguments.length > 1)
+    ? curriedBind.apply(this, [].slice.call(arguments, 1))
     : curriedBind
 }
 
 export const curry = fn => {
-  return function (...args) {
-    const g = bind(fn).apply(fn, [this].concat(args))
-    return args.length < fn.length ? curry(g) : g()
+  const a = function () {
+    const g = bind(fn).apply(fn, [this].concat([].slice.call(arguments)))
+    return !functionLength(g) ? g() : curry(g)
+  }
+
+  a._length = functionLength(fn)
+
+  return a
+}
+
+const _groupsOf = curry((n, xs) => {
+  if (!xs.length) return []
+  return [xs.slice(0,n)].concat(_groupsOf(n, xs.slice(n, xs.length)))
+})
+
+const _compose = curry((f,g,x) => f(g(x)))
+const toAssociativeCommaInfix = fn => {
+  return function () {
+    const fns = [].slice.call(arguments)
+    return function () {
+      return _groupsOf(2, fns).reverse().map(g => {
+        return g.length > 1 ? fn.apply(this,g) : g[0]
+      }).reduce((x, f) => [f.apply(f, x)], arguments)[0]
+    }
   }
 }
 
-export const compose = (...args) => ctx => args.reverse().reduce((ctx, fn) => fn(ctx), ctx)
+export const compose = toAssociativeCommaInfix(_compose)
 
 export const Functor = value => {
   return {
-    map: (fn) => fn(value),
+    map: (fn) => Functor(fn(value)),
     valueOf: () => value,
-    chain: () => value,
+    chain: (fn) => Functor(fn(value)),
     toString: () => `Functor(${value})`
   }
 }
 
 export const Maybe = value => {
   return {
-    map: (fn) => value ? Functor(fn(value)) : Maybe(null),
+    map: (fn) => value ? Maybe(fn(value)) : Maybe(null),
     valueOf: () => value,
-    chain: () => value,
+    chain: (fn) => value ? fn(value) : Maybe(null),
     of: value => Maybe(value),
     toString: () => `Maybe(${value})`
   }
 }
 
-export const MaybeCondition = curry((condition, value) => {
+export const MaybeCondition = curry(function (condition, value) {
   return {
-    map: (fn) => condition(value) ? Functor(fn(value)) : Maybe(null),
+    map: (fn) => condition(value) ? Maybe(fn(value)) : Maybe(null),
     valueOf: () => value,
-    chain: () => value,
+    chain: (fn) => condition(value) ? fn(value) : Maybe(null),
     of: value => MaybeCondition(condition, value),
     toString: () => `MaybeCondition(${value})`
   }
@@ -54,6 +82,9 @@ export const map = curry((fn, obj) => {
 
 export const identity = v => v
 
-export const chain = curry((fn, obj) => obj.chain ? obj.chain(fn) : obj.then(fn))
+export const chain = curry((fn, obj) => {
+  return obj.chain ? obj.chain(fn) : obj.then(fn)
+})
+
 export const mjoin = obj => chain(identity, obj)
 
