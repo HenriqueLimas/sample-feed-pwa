5b688a3d1d8473688a89f4e7bd4d2b2e56c24b13
HenriqueLimas <henrique.ramos.limas@gmail.com>
2017-07-09 19:58:43 -0300

    build(css): add support for others styles files

--- build/transpile-css.js
+++ build/transpile-css.js
@@ -19,32 +19,46 @@ const processor = postcss([
 ])
 
 const appShellEntries = [
-  'client/styles/app.css'
+  'app.css'
 ]
 
-const BASE_PATH = 'src/'
+const BASE_PATH = 'src/client/styles/'
+
+const styles = [
+  'login.css'
+]
+
+const processCss = entry => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(BASE_PATH + entry, (err, css) => {
+      if (err) return reject(err)
+
+      processor.process(css, { in: BASE_PATH + entry, out: `dist/client/styles/${entry}` })
+        .then(resolve)
+        .catch(reject)
+    })
+  })
+}
+
+const writeFile = fileName => results => {
+  const css = results.reduce((css, result) => css + result.css, '')
+  const map = results.reduce((map, result) => result.map ? map + result.map : map, '')
+
+  fs.writeFile(`dist/client/styles/${fileName}`, css)
+  if (map) fs.writeFile(`dist/client/styles/${fileName}.map`, map)
+}
 
 mkdirp('dist/client/styles', err => {
   if (err) return console.error(err)
 
   Promise.all(
-    appShellEntries.map(entry => {
-      return new Promise((resolve, reject) => {
-        fs.readFile(BASE_PATH + entry, (err, css) => {
-          if (err) return reject(err)
+    appShellEntries.map(processCss)
+  ).then(writeFile('app-shell.css'))
 
-          processor.process(css, { in: BASE_PATH + entry, out: `dist/${entry}` })
-            .then(resolve)
-            .catch(reject)
-        })
-      })
+  styles
+    .map(entry => {
+      return processCss(entry)
+        .then(result => writeFile(entry)([result]))
     })
-  ).then(results => {
-    const css = results.reduce((css, result) => css + result.css, '')
-    const map = results.reduce((map, result) => result.map ? map + result.map : map, '')
-
-    fs.writeFile('dist/client/styles/app-shell.css', css)
-    if (map) fs.writeFile('dist/client/styles/app-shell.css.map', map)
-  })
 })
 
